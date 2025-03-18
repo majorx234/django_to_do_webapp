5f301dc2fc109cd88844f0f2914e5874771feaf2
Majorx234 <majorx234@googlemail.com>
2025-03-18 23:55:45 +0100

    Add views for todo app

--- todo/todo_manager/todo/views.py
+++ todo/todo_manager/todo/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic import CreateView, UpdateView
+
+from todo.models import Task
+from todo.forms import ToDoForm
 
 # Create your views here.
+class ToDoCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    permission_required = 'todo.add_task'
+    model = Task
+    template_name = 'detail.html'
+    form_class = ToDoForm
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context['TITLE_SHORT'] = 'Edit'
+        context['TITLE_LONG'] = 'Edit a task'
+        return context
+
+class ToDoListView(LoginRequiredMixin, PermissionRequiredMixin):
+    permission_required = 'Aufgaben.view_aufgabe'
+    model = Aufgabe
+    template_name = 'liste.html'
+    table_class = ToDoTable
