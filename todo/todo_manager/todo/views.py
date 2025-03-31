f7f2d6afe0032501b09fc1bc78cc42df0ae55e67
Majorx234 <majorx234@googlemail.com>
2025-03-31 23:40:47 +0200

    Add class based views for formular input and prepare list view

--- todo/todo_manager/todo/views.py
+++ todo/todo_manager/todo/views.py
@@ -1,8 +1,35 @@
-from django.views.generic import View
+from django.views.generic import CreateView, DetailView, View
+from django_tables2.views import SingleTableMixin
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from todo.models import Task
+from todo.forms import ToDoForm
 from todo.tables import ToDoTable
+
+
+# Create your views here.
+class ToDoCreateView(CreateView):
+    # permission_required = 'todo.add_task'
+    model = Task
+    template_name = 'detail.html'
+    form_class = ToDoForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+
+        return super().form_valid(form)
+
+
+class ToDoListView(DetailView):
+    # permission_required = 'todo.view_tasks'
+    model = Task
+    template_name = 'list.html'
+    #    table_class = ToDoTable
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
 
 
 class MainView(View):
@@ -14,4 +41,4 @@ class MainView(View):
     table_class = ToDoTable
 
     def get(self, request, *args, **kwargs):
-        return render(request,'current_todo.html')
+        return render(request, 'current_todo.html')
