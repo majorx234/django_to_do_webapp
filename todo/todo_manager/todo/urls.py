f644fab2e99d42333cf37f33d5f9ecd558744015
Majorx234 <majorx234@googlemail.com>
2025-04-08 23:58:49 +0200

    Add view to create todos update detail view

--- todo/todo_manager/todo/urls.py
+++ todo/todo_manager/todo/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 # from django.conf.urls import urls
 
-from .views import MainView, ToDoCreateView, ToDoListView, ToDoUpdateView
+from .views import MainView, ToDoCreateView, \
+                   ToDoListView, ToDoUpdateView, \
+                   ToDoDetailView
+
 
 urlpatterns = [
     path('', MainView.as_view(), name='main'),
     path('create/', ToDoCreateView.as_view(), name='create'),
+    path('detail/', ToDoDetailView.as_view(), name='detail'),
     path('update/', ToDoUpdateView.as_view(), name='update'),
     path('list/', ToDoListView.as_view(), name='list'),
 ]
--- todo/todo_manager/todo/views.py
+++ todo/todo_manager/todo/views.py
@@ -12,15 +12,27 @@ from todo.tables import ToDoTable
 class ToDoCreateView(CreateView):
     # permission_required = 'todo.add_task'
     model = Task
-    template_name = 'detail.html'
+    template_name = 'create.html'
     form_class = ToDoForm
-    success_url = '/'
+    # success_url = '/'
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
 
         return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['METHOD'] = 'create'
+        return context
+
+
+class ToDoDetailView(DetailView):
+    # permission_required = 'todo.view_tasks'
+    model = Task
+    template_name = 'detail.html'
 
 
 class ToDoListView(DetailView):
